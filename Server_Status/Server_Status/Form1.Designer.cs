11f99d13f04f530de3a565c6a54a36a7b651923b
Harm Haandrikman <69154555+NietTeDoenDlive@users.noreply.github.com>
2023-03-14 13:59:12 +0100

    update V1.0
    the ping does work now

--- Server_Status/Server_Status/Form1.Designer.cs
+++ Server_Status/Server_Status/Form1.Designer.cs
@@ -29,57 +29,73 @@
         private void InitializeComponent()
         {
             this.Check_BTN = new System.Windows.Forms.Button();
-            this.URL = new System.Windows.Forms.TextBox();
+            this.URL_TXT = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.Status_LBL = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // Check_BTN
             // 
-            this.Check_BTN.Location = new System.Drawing.Point(242, 55);
+            this.Check_BTN.Location = new System.Drawing.Point(374, 20);
             this.Check_BTN.Name = "Check_BTN";
-            this.Check_BTN.Size = new System.Drawing.Size(94, 31);
+            this.Check_BTN.Size = new System.Drawing.Size(109, 31);
             this.Check_BTN.TabIndex = 0;
             this.Check_BTN.Text = "Check";
             this.Check_BTN.UseVisualStyleBackColor = true;
+            this.Check_BTN.Click += new System.EventHandler(this.Check_BTN_Click);
             // 
-            // URL
+            // URL_TXT
             // 
-            this.URL.Location = new System.Drawing.Point(12, 22);
-            this.URL.Name = "URL";
-            this.URL.PlaceholderText = "URL";
-            this.URL.Size = new System.Drawing.Size(324, 27);
-            this.URL.TabIndex = 1;
+            this.URL_TXT.Location = new System.Drawing.Point(12, 22);
+            this.URL_TXT.Name = "URL_TXT";
+            this.URL_TXT.PlaceholderText = "URL";
+            this.URL_TXT.Size = new System.Drawing.Size(356, 27);
+            this.URL_TXT.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.Size = new System.Drawing.Size(91, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Server Status: ";
             // 
-            // label2
+            // Status_LBL
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(119, 60);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Null";
+            this.Status_LBL.AutoSize = true;
+            this.Status_LBL.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Status_LBL.Location = new System.Drawing.Point(110, 52);
+            this.Status_LBL.Name = "Status_LBL";
+            this.Status_LBL.Size = new System.Drawing.Size(32, 17);
+            this.Status_LBL.TabIndex = 3;
+            this.Status_LBL.Text = "Null";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(466, 124);
+            this.listBox1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(343, 97);
-            this.Controls.Add(this.label2);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(495, 215);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Status_LBL);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.URL);
+            this.Controls.Add(this.URL_TXT);
             this.Controls.Add(this.Check_BTN);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,8 +104,9 @@
         #endregion
 
         private Button Check_BTN;
-        private TextBox URL;
+        private TextBox URL_TXT;
         private Label label1;
-        private Label label2;
+        private Label Status_LBL;
+        private ListBox listBox1;
     }
 }
--- Server_Status/Server_Status/Form1.cs
+++ Server_Status/Server_Status/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Net.NetworkInformation;
+
 namespace Server_Status
 {
     public partial class Form1 : Form
@@ -5,6 +8,42 @@ namespace Server_Status
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Check_BTN_Click(object sender, EventArgs e)
+        {
+            Status_LBL.Text = "trying...";
+            try
+            {
+                string Hostname = URL_TXT.Text;
+                Ping ping = new();
+                PingReply reply = ping.Send(Hostname);
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.WriteLine("Success");
+                    Status_LBL.ForeColor = Color.DarkGreen;
+                    Status_LBL.Text = reply.Status.ToString();
+                    listBox1.Items.Add(Hostname + " | " + reply.Status + " | "+ reply.RoundtripTime + "ms" + " | " + DateTime.Now);
+                }
+                else
+                {
+                    Console.WriteLine("failed");
+                    Status_LBL.ForeColor = Color.DarkRed;
+                    Status_LBL.Text = reply.Status.ToString();
+                    listBox1.Items.Add(Hostname + " | " + reply.Status + " | " + DateTime.Now);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+                Status_LBL.ForeColor = Color.DarkRed;
+                Status_LBL.Text = ex.Message;
+            }
         }
     }
 }
